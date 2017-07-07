ea039fdb1e184724a47eb3fbf0b636a8e6f724af
Vladimir Alarcon <valarcon>
2017-07-07 16:05:23 -0400

    HOT-95 - Fixed escaping special symbols.

--- hotrod/src/main/java/org/hotrod/config/dynamicsql/VerbatimSQLPart.java
+++ hotrod/src/main/java/org/hotrod/config/dynamicsql/VerbatimSQLPart.java
@@ -11,7 +11,7 @@ public class VerbatimSQLPart extends DynamicSQLPart {
 
   @Override
   public String render() {
-    return this.text;
+    return this.text.replace("&", "&amp;").replace("<", "&lt;").replace("\"", "&quot;");
   }
 
 }
