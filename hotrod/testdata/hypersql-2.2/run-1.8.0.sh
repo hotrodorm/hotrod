58386785002514040671ff94e81d4621d8ab32f9
Vladimir Alarcon <valarcon@hotrod.com>
2017-08-25 10:59:04 -0400

    HOT-118 - JDBC drivers clean up.

--- hotrod/testdata/hypersql-2.2/run-1.8.0.sh
+++ hotrod/testdata/hypersql-2.2/run-1.8.0.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-java -cp jdbc-drivers/hsqldb-1.8.0.jar org.hsqldb.Server
+mkdir -p db0
+cd db0
+java -cp ../jdbc-drivers/hsqldb-1.8.0.jar org.hsqldb.Server
 
 
--- hotrod/testdata/hypersql-2.2/run-2.3.4.sh
+++ hotrod/testdata/hypersql-2.2/run-2.3.4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p db2
-java -cp jdbc-drivers/hsqldb-2.3.4.jar org.hsqldb.server.Server --database.0 file:db2/db2 --dbname.0 xdb
+java -cp jdbc-drivers/hsqldb-2.3.4.jar org.hsqldb.server.Server --database.0 file:db3/db3 --dbname.0 xdb
 
