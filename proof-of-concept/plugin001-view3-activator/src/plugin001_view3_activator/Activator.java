f53ef9a40717228c3bc6e8ba2aa63df553a75af3
valarcon <valarcon>
2018-01-03 16:15:19 -0500

    Changing name.

--- proof-of-concept/plugin001-view3-activator/src/plugin001_view3_activator/Activator.java
+++ proof-of-concept/plugin001-view3-activator/src/plugin001_view3_activator/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "plugin001-view3-activator"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "hotrod-eclipse-plugin-poc001"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
