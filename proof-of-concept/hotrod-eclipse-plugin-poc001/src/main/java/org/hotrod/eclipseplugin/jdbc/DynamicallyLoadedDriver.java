23d7f2ba9528edd77cd932344b46b89bc3a12584
valarcon <valarcon>
2018-01-24 13:10:09 -0500

    HOT-27 - Fully upgraded to Java 8.

--- proof-of-concept/hotrod-eclipse-plugin-poc001/src/main/java/org/hotrod/eclipseplugin/jdbc/DynamicallyLoadedDriver.java
+++ proof-of-concept/hotrod-eclipse-plugin-poc001/src/main/java/org/hotrod/eclipseplugin/jdbc/DynamicallyLoadedDriver.java
@@ -6,8 +6,10 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import org.eclipse.core.resources.IProject;
 
@@ -105,6 +107,11 @@ public class DynamicallyLoadedDriver {
     @Override
     public boolean jdbcCompliant() {
       return this.driver.jdbcCompliant();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+      return this.driver.getParentLogger();
     }
 
   }
