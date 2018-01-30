a760ab93796c31f886fc599b9e6585827ec1f933
Vladimir Alarcon <valarcon@hotrod.com>
2018-01-29 20:56:40 -0500

    HOT-27 - Adding correlator comparator.

--- proof-of-concept/hotrod-eclipse-plugin-poc001/src/main/java/org/hotrod/eclipseplugin/utils/Correlator.java
+++ proof-of-concept/hotrod-eclipse-plugin-poc001/src/main/java/org/hotrod/eclipseplugin/utils/Correlator.java
@@ -3,6 +3,7 @@ package org.hotrod.eclipseplugin.utils;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -51,7 +52,6 @@ public class Correlator {
 
     while (l != null && r != null) {
       int comp = l.compareTo(r);
-      // display("left=" + left + " right=" + right + " comp=" + comp);
       if (comp == 0) {
         result.add(new CorrelatedEntry<T>(l, r));
         l = lit.hasNext() ? lit.next() : null;
@@ -79,7 +79,109 @@ public class Correlator {
 
   }
 
+  public static <T> List<CorrelatedEntry<T>> correlateUnsorted(final Collection<T> left, final Collection<T> right,
+      final Comparator<T> comparator) {
+
+    if (comparator == null) {
+      throw new NullPointerException("comparator is null");
+    }
+
+    List<T> sortedLeft = new ArrayList<T>(left);
+    Collections.sort(sortedLeft, comparator);
+
+    List<T> sortedRight = new ArrayList<T>(right);
+    Collections.sort(sortedRight, comparator);
+
+    return correlateSorted(sortedLeft, sortedRight, comparator);
+
+  }
+
+  public static <T> List<CorrelatedEntry<T>> correlateSorted(final Collection<T> left, final Collection<T> right,
+      final Comparator<T> comparator) {
+
+    if (comparator == null) {
+      throw new NullPointerException("comparator is null");
+    }
+
+    List<CorrelatedEntry<T>> result = new ArrayList<CorrelatedEntry<T>>();
+
+    if (left == null && right == null) {
+      return result;
+    }
+
+    if (left == null && right != null) {
+      for (T r : right) {
+        result.add(new CorrelatedEntry<T>(null, r));
+      }
+      return result;
+    }
+
+    if (left != null && right == null) {
+      for (T l : left) {
+        result.add(new CorrelatedEntry<T>(l, null));
+      }
+      return result;
+    }
+
+    Iterator<T> lit = left.iterator();
+    Iterator<T> rit = right.iterator();
+
+    Entry<T> l = read(lit);
+    Entry<T> r = read(rit);
+
+    while (l.read && r.read) {
+      T le = l.element;
+      T re = r.element;
+      int comp = comparator.compare(le, re);
+      if (comp == 0) {
+        result.add(new CorrelatedEntry<T>(le, re));
+        l = read(lit);
+        r = read(rit);
+      } else if (comp < 0) {
+        result.add(new CorrelatedEntry<T>(le, null));
+        l = read(lit);
+      } else {
+        result.add(new CorrelatedEntry<T>(null, re));
+        r = read(rit);
+      }
+    }
+
+    while (l.read) {
+      T le = l.element;
+      result.add(new CorrelatedEntry<T>(le, null));
+      l = read(lit);
+    }
+
+    while (r.read) {
+      T re = r.element;
+      result.add(new CorrelatedEntry<T>(null, re));
+      r = read(rit);
+    }
+
+    return result;
+
+  }
+
+  private static <T> Entry<T> read(final Iterator<T> iterator) {
+    if (iterator.hasNext()) {
+      return new Entry<T>(true, iterator.next());
+    } else {
+      return new Entry<T>(false, null);
+    }
+  }
+
   // Classes
+
+  private static class Entry<T> {
+
+    public boolean read;
+    public T element;
+
+    public Entry(boolean read, T t) {
+      this.read = read;
+      this.element = t;
+    }
+  }
 
   public static class CorrelatedEntry<T> {
 
--- proof-of-concept/hotrod-eclipse-plugin-poc001/src/test/java/automated/CorrelatorTests.java
+++ proof-of-concept/hotrod-eclipse-plugin-poc001/src/test/java/automated/CorrelatorTests.java
@@ -1,6 +1,7 @@
 package automated;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import org.hotrod.eclipseplugin.utils.Correlator;
@@ -14,7 +15,7 @@ public class CorrelatorTests extends TestCase {
     super(txt);
   }
 
-  public void testSorted() {
+  public void testSortedComparable() {
 
     {
       List<String> left = null;
@@ -105,6 +106,160 @@ public class CorrelatorTests extends TestCase {
       assertEquals(null, ce.get(1).getRight());
       assertEquals(null, ce.get(2).getLeft());
       assertEquals("ghi", ce.get(2).getRight());
+    }
+
+  }
+
+  public void testSortedComparator() {
+
+    Comparator<Row> comp = Row.getComparator();
+
+    {
+      List<Row> left = null;
+      List<Row> right = null;
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(0, ce.size());
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] {});
+      List<Row> right = null;
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(0, ce.size());
+    }
+
+    {
+      List<Row> left = null;
+      List<Row> right = Arrays.asList(new Row[] {});
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(0, ce.size());
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] {});
+      List<Row> right = Arrays.asList(new Row[] {});
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(0, ce.size());
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] { null }); // TODO: fix
+      List<Row> right = Arrays.asList(new Row[] {});
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(1, ce.size());
+      assertEquals(null, ce.get(0).getLeft());
+      assertEquals(null, ce.get(0).getRight());
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] { new Row(1, "abc") });
+      List<Row> right = Arrays.asList(new Row[] {});
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(1, ce.size());
+      assertTrue(ce.get(0).getLeft().is(1, "abc"));
+      assertEquals(null, ce.get(0).getRight());
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] {});
+      List<Row> right = Arrays.asList(new Row[] { new Row(2, "def") });
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(1, ce.size());
+      assertEquals(null, ce.get(0).getLeft());
+      assertTrue(ce.get(0).getRight().is(2, "def"));
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] { new Row(1, "abcL") });
+      List<Row> right = Arrays.asList(new Row[] { new Row(1, "abcD") });
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(1, ce.size());
+      assertTrue(ce.get(0).getLeft().is(1, "abcL"));
+      assertTrue(ce.get(0).getRight().is(1, "abcD"));
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] { new Row(1, "abc") });
+      List<Row> right = Arrays.asList(new Row[] { new Row(2, "def") });
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(2, ce.size());
+
+      assertTrue(ce.get(0).getLeft().is(1, "abc"));
+      assertEquals(null, ce.get(0).getRight());
+
+      assertEquals(null, ce.get(1).getLeft());
+      assertTrue(ce.get(1).getRight().is(2, "def"));
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] { new Row(2, "def") });
+      List<Row> right = Arrays.asList(new Row[] { new Row(1, "abc") });
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(2, ce.size());
+
+      assertEquals(null, ce.get(0).getLeft());
+      assertTrue(ce.get(0).getRight().is(1, "abc"));
+
+      assertTrue(ce.get(1).getLeft().is(2, "def"));
+      assertEquals(null, ce.get(1).getRight());
+    }
+
+    {
+      List<Row> left = Arrays.asList(new Row[] { new Row(2, "def") });
+      List<Row> right = Arrays.asList(new Row[] { new Row(1, "abc"), new Row(3, "ghi") });
+      List<CorrelatedEntry<Row>> ce = Correlator.correlateSorted(left, right, comp);
+      assertEquals(3, ce.size());
+
+      assertEquals(null, ce.get(0).getLeft());
+      assertTrue(ce.get(0).getRight().is(1, "abc"));
+
+      assertTrue(ce.get(1).getLeft().is(2, "def"));
+      assertEquals(null, ce.get(1).getRight());
+
+      assertEquals(null, ce.get(2).getLeft());
+      assertTrue(ce.get(2).getRight().is(3, "ghi"));
+    }
+
+  }
+
+  // Classes
+
+  public static class Row {
+
+    private int key;
+    private String property1;
+
+    public Row(int key, String property1) {
+      this.key = key;
+      this.property1 = property1;
+    }
+
+    public int getKey() {
+      return key;
+    }
+
+    public String getProperty1() {
+      return property1;
+    }
+
+    public String toString() {
+      return "[key:" + this.key + ", property1:" + this.property1 + "]";
+    }
+
+    public boolean is(final int key, final String property1) {
+      if (this.key != key) {
+        return false;
+      }
+      return this.property1 == null ? property1 == null : this.property1.equals(property1);
+    }
+
+    public static Comparator<Row> getComparator() {
+      return new Comparator<Row>() {
+        @Override
+        public int compare(final Row o1, final Row o2) {
+          return o1.key - o2.key;
+        }
+      };
     }
 
   }
