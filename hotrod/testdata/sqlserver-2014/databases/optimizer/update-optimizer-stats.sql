10827c02eb692c10a0dc6be91b4fbbb6ff3a9d11
valarcon <valarcon>
2018-02-12 12:01:35 -0500

    Adding SQL Server & SAP ASE to optimizer data.

---
+++ hotrod/testdata/sqlserver-2014/databases/optimizer/update-optimizer-stats.sql
@@ -0,0 +1,11 @@
+-- Update table statistics for the SQL optimizer
+
+update statistics code;
+update statistics address;
+update statistics shipment;
+update statistics product;
+update statistics customer;
+update statistics "order";
+update statistics order_item;
+
+
