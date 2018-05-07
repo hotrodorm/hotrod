b8ca31de59d065e0a6833a4038ac7dc9fc6ffc92
valarcon <valarcon>
2018-05-07 11:47:41 -0400

    HOT27- Changed plugin ID.

--- hotrod/src/eclipseplugin/java/org/hotrod/eclipseplugin/Activator.java
+++ hotrod/src/eclipseplugin/java/org/hotrod/eclipseplugin/Activator.java
@@ -11,7 +11,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
   // The plug-in ID
-  public static final String PLUGIN_ID = "hotrod-eclipse-plugin"; //$NON-NLS-1$
+  public static final String PLUGIN_ID = "org.hotrod.eclipse"; //$NON-NLS-1$
 
   // The shared instance
   private static Activator plugin;
