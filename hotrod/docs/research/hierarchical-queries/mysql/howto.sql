c525d7f35bd864b4bea1a81c8c676167db9910a8
Vladimir Alarcon <valarcon@hotrod.com>
2018-06-05 19:58:25 -0400

    HOT27 - Documenting recursive joins.

--- hotrod/docs/research/hierarchical-queries/mysql/howto.sql
+++ hotrod/docs/research/hierarchical-queries/mysql/howto.sql
@@ -1,1 +1,1 @@
--- Supports wil start in version 8.0. Not yet ready.
+-- Supports will start in version 8.0. Not yet ready.
---
+++ hotrod/docs/research/hierarchical-queries/oracle/howto.sql
@@ -0,0 +1,34 @@
+-- === Oracle 12c ===
+
+drop table shopper;
+
+create table shopper (
+  id int primary key not null,
+  name varchar(20) not null,
+  referred_by_id int,
+  constraint fk1 foreign key (referred_by_id) references shopper (id)
+);
+
+insert into shopper (id, name, referred_by_id) values (1, 'Peter', null);
+insert into shopper (id, name, referred_by_id) values (2, 'Anna', 1);
+insert into shopper (id, name, referred_by_id) values (3, 'Mary', null);
+insert into shopper (id, name, referred_by_id) values (4, 'John', 1);
+insert into shopper (id, name, referred_by_id) values (5, 'Donna', 3);
+insert into shopper (id, name, referred_by_id) values (6, 'Michael', 4);
+
+with referrer (id, referred_by_id, client, original) as (
+    select id, referred_by_id, name, name from shopper where substr(name, 1, 1) = 'M'
+    union all
+    select s.id, s.referred_by_id, r.client, s.name from referrer r join shopper s on s.id = r.referred_by_id
+)
+select * from referrer where referred_by_id is null order by client;
+
+-- Recursive WITH clause:
+--  * Must have a query name.
+--  * Must have column aliases.
+--  * Must use UNION ALL to separate the anchor member from the recursive member.
+--  * The anchor member cannot reference the query name.
+--  * The recursive member must include a single reference to the query name.
+
+
+
