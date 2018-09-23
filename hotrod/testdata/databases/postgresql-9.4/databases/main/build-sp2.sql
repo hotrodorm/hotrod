a024a08f14a51663b21900c5529b6e64de08767b
Vladimir Alarcon <valarcon@hotrod.com>
2018-09-23 12:19:42 -0400

    Adding SQL def.

--- hotrod/testdata/databases/postgresql-9.4/databases/main/build-sp2.sql
+++ hotrod/testdata/databases/postgresql-9.4/databases/main/build-sp2.sql
@@ -1,10 +1,63 @@
-create function employee_state_read_only() returns trigger as $sp_delimiter$
+create function employee_state_read_only() returns trigger as $BODY$
 begin
     raise exception 'Insert, update, or delete operation not allowed on read-only table "employee_state".';
 end;
-$sp_delimiter$ language plpgsql;
+$BODY$ language plpgsql;
 //
 
 create trigger employee_state_read_only before insert or update or delete on employee_state
   for each statement execute procedure employee_state_read_only();
 //
+
+create or replace function increment(i int) returns int as $BODY$
+begin
+  return i + 1;
+end;
+$BODY$ language plpgsql;
+//
+
+create or replace function lookup() 
+  returns table (id int, name varchar)
+as $BODY$
+begin
+  return query
+  select a.id, a.name from account a;
+end;
+$BODY$ language plpgsql;
+//
+
+create or replace function lookupAccountTxs() 
+  returns refcursor
+as $BODY$
+declare ref refcursor;
+begin
+  open ref for
+  select a.*, t.amount 
+    from account a
+    join transaction t on t.account_id = a.id;
+  return ref;
+end;
+$BODY$ language plpgsql;
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
