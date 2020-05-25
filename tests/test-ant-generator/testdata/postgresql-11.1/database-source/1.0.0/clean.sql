7a1907674e997335898ae3934ab099c29024be1f
valarcon <valarcon@example.com>
2020-05-24 22:42:33 -0400

    Purge tested in Maven and Ant.

---
+++ tests/test-ant-generator/testdata/postgresql-11.1/database-source/1.0.0/clean.sql
@@ -0,0 +1,77 @@
+drop table if exists product;
+
+drop table if exists cursor_example1;
+
+drop sequence if exists "<Stock$".seq_price;
+
+drop table if exists "<Stock$"."&Price%";
+drop table if exists "house_ROOM";
+drop table if exists schema2.account_alert;
+drop table if exists house;
+drop table if exists schema2.house;
+drop view if exists schema2.low_account;
+
+drop table if exists "car#part$Price";
+
+drop table if exists vehicle;
+drop table if exists vehicle_type;
+
+drop view if exists tx_branch;
+
+drop table if exists application_config;
+
+drop table if exists codes; 
+drop table if exists quadrant; 
+
+drop table if exists agent; 
+drop table if exists client;
+drop table if exists transaction; 
+drop table if exists federal_branch; 
+drop table if exists state_branch; 
+drop table if exists account; 
+
+drop table if exists config_values; 
+drop table if exists properties; 
+drop table if exists parameters; 
+drop table if exists log; 
+
+drop sequence if exists seq_agent; 
+
+drop table if exists island;
+
+drop table if exists event; 
+
+-- Auto-generated columns 
+
+drop table if exists test_sequence1;
+drop table if exists test_sequence2;
+drop table if exists test_identity1;
+drop sequence if exists gen_seq1;
+drop sequence if exists gen_seq2;
+drop table if exists test_mixed1;
+drop table if exists test_seq_ide_def1;
+drop table if exists test_identity2;
+drop table if exists test_default1;
+drop table if exists test_default2;
+
+-- All Types
+
+drop table if exists item;
+
+drop table if exists types_numeric;
+drop table if exists types_char;
+drop table if exists types_binary;
+drop table if exists types_date_time;
+drop table if exists types_other;  
+
+drop type if exists mood;
+drop type if exists complex;
+
+drop table if exists employee;
+
+drop trigger employee_state_read_only on employee_state;
+drop function employee_state_read_only;
+drop table if exists employee_state;
+
+
+
---
+++ tests/test-ant-generator/testdata/postgresql-11.1/database-source/1.0.0/scenarios/unit-testing/clean-data.sql
@@ -0,0 +1,18 @@
+delete from application_config;
+
+delete from client;
+
+delete from transaction;
+delete from account;
+delete from federal_branch;
+delete from state_branch;
+
+delete from config_values;
+delete from properties;
+
+delete from vehicle;
+
+-- enum
+
+delete from employee;
+delete from employee_state;
