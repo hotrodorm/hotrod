0edef040f2a235b5437f207d6d0e8f45d6e56cc7
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2023-01-11 17:41:01 -0500

    All types ready.

--- tests/test-aurora/src/main/java/test/App.java
+++ tests/test-aurora/src/main/java/test/App.java
@@ -2,11 +2,15 @@ package test;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 
 import org.hotrod.runtime.livesql.LiveSQL;
 import org.mybatis.spring.annotation.MapperScan;
+import org.postgresql.util.PGobject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +23,9 @@ import org.springframework.context.annotation.Configuration;
 import test.persistence.EmployeeVO;
 import test.persistence.TypesBinaryVO;
 import test.persistence.TypesCharVO;
+import test.persistence.TypesDateTimeVO;
 import test.persistence.TypesNumericVO;
+import test.persistence.TypesOtherVO;
 import test.persistence.primitives.EmployeeDAO;
 import test.persistence.primitives.EmployeeDAO.EmployeeTable;
 import test.persistence.primitives.TypesBinaryDAO;
@@ -69,7 +75,7 @@ public class App {
     };
   }
 
-  private void searching() {
+  private void searching() throws SQLException {
 
     // Use CRUD to search for employee #123
 
@@ -143,6 +149,55 @@ public class App {
     for (TypesBinaryVO c : this.typesBinaryDAO.selectByExample(new TypesBinaryVO())) {
       System.out.println("b=" + tb);
       System.out.println("bin1=" + tb.getBin1()[0] + tb.getBin1()[1] + tb.getBin1()[2] + tb.getBin1()[3]);
+    }
+
+    // Inserting in types_date_time
+
+    this.typesDateTimeDAO.deleteByExample(new TypesDateTimeVO());
+
+    TypesDateTimeVO td = new TypesDateTimeVO();
+
+    Timestamp ts = new Timestamp(System.currentTimeMillis());
+
+    td.setId(1);
+    td.setDat1(new Date(System.currentTimeMillis()));
+    td.setTs1(ts);
+    td.setTs2(ts);
+    td.setTs3(ts);
+    td.setTs4(ts);
+    td.setTs5(ts);
+    td.setTim1(ts);
+    td.setTim2(ts);
+    td.setTim3(ts);
+    td.setTim4(ts);
+    td.setTim5(ts);
+
+    this.typesDateTimeDAO.insert(td);
+    for (TypesDateTimeVO d : this.typesDateTimeDAO.selectByExample(new TypesDateTimeVO())) {
+      System.out.println("d=" + d);
+    }
+
+    // Inserting in types_other
+
+    TypesOtherVO to = new TypesOtherVO();
+
+    PGobject jo = new PGobject();
+    jo.setType("json");
+    jo.setValue("{ \"a\" : 1}");
+
+    to.setJso1(jo);
+    to.setJso2(jo);
+
+    int[] ia = new int[] { 12, 34 };
+//    to.setArr1(ia);
+
+    char[] ca = new char[] { 'a', 'b' };
+//    to.setArr2(ca);
+
+    this.typesOtherDAO.insert(to);
+
+    for (TypesOtherVO o : this.typesOtherDAO.selectByExample(new TypesOtherVO())) {
+      System.out.println("o=" + o);
     }
 
   }
