53a7abcb32ee8b1bd4d36e0d45bc3d1292dc612d
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2023-04-22 12:41:02 -0400

    Adding tests for 'vo' attributes in entities.

--- tests/test-foreach/src/main/java/app/App.java
+++ tests/test-foreach/src/main/java/app/App.java
@@ -2,13 +2,8 @@ package app;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.hotrod.runtime.livesql.LiveSQL;
-import org.hotrod.runtime.livesql.queries.DeleteWherePhase;
-import org.hotrod.runtime.livesql.queries.UpdateSetCompletePhase;
-import org.hotrod.runtime.livesql.queries.select.CriteriaWherePhase;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,11 +14,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import app.daos.Car_part_priceVO;
-import app.daos.PlayerVO;
-import app.daos.primitives.Car_part_priceDAO;
-import app.daos.primitives.Car_part_priceDAO.Car_part_priceTable;
-import app.daos.primitives.PlayerDAO;
+import app.daos.BranchVO;
+import app.daos.LowBranchVO;
+import app.daos.primitives.BranchDAO;
+import app.daos.primitives.QueriesBDAO;
 
 @Configuration
 @SpringBootApplication
@@ -38,11 +32,17 @@ public class App {
 //  @Autowired
 //  private EmployeeDAO employeeDAO;
 
-  @Autowired
-  private PlayerDAO playerDAO;
+//  @Autowired
+//  private PlayerDAO playerDAO;
+//
+//  @Autowired
+//  private Car_part_priceDAO cppDAO;
 
   @Autowired
-  private Car_part_priceDAO cppDAO;
+  private BranchDAO branchDAO;
+
+  @Autowired
+  private QueriesBDAO queriesBDAO;
 
   @Autowired
   private LiveSQL sql;
@@ -57,57 +57,74 @@ public class App {
       System.out.println("[ Starting example ]");
 //      converterPGArray();
 //      updateByCriteria();
-      deleteByCriteria();
+//      deleteByCriteria();
+      selectEntity();
 //      converter();
 //      forEach();
       System.out.println("[ Example complete ]");
     };
   }
 
-  private void converterPGArray() {
-    PlayerVO p = this.playerDAO.select(101);
-    System.out.println("> Player:" + p.getId() + " cards="
-        + Stream.of(p.getCards()).map(c -> "" + c).collect(Collectors.joining("|")));
-    p.setCards(new Integer[] { 7, 8, 8, 9 });
-    this.playerDAO.update(p);
-    System.out.println("> Player updated.");
-  }
+  private void selectEntity() {
 
-  private void updateByCriteria() {
-    System.out.println("> updateByCriteria()");
-    Car_part_priceTable p = Car_part_priceDAO.newTable("p");
+    List<BranchVO> branches = this.branchDAO.findTop3Branches();
+    System.out.println("> Top 3 branches: ");
+    for (BranchVO b : branches) {
+      System.out.println("> b=" + b);
+    }
 
-    Car_part_priceVO updateValues = new Car_part_priceVO();
-    updateValues.set_discount(250);
-    updateValues.setPrice_dollar(150);
-    UpdateSetCompletePhase u = this.cppDAO.update(updateValues, p, p.part_.eq(1));
-    System.out.println("Update SQL:" + u.getPreview());
-    u.execute();
-
-    for (Car_part_priceVO vo : this.cppDAO.select(p, p.part_.eq(p.part_)).execute()) {
-      System.out.println("> CPP=" + vo);
+    List<LowBranchVO> lowBranches = this.queriesBDAO.findLowBranches();
+    System.out.println("> Low branches: ");
+    for (LowBranchVO lb : lowBranches) {
+      System.out.println("> lb=" + lb);
     }
 
   }
 
-  private void deleteByCriteria() {
-    System.out.println("> deleteByCriteria()");
-    Car_part_priceTable p = Car_part_priceDAO.newTable("p");
-
-    Car_part_priceVO updateValues = new Car_part_priceVO();
-    updateValues.set_discount(250);
-    updateValues.setPrice_dollar(150);
-    DeleteWherePhase d = this.cppDAO.delete(p, p.part_.eq(2));
-    System.out.println("Delete SQL:" + d.getPreview());
-    d.execute();
-
-    CriteriaWherePhase<Car_part_priceVO> s2 = this.cppDAO.select(p, p.part_.eq(p.part_));
-    System.out.println("Select SQL:" + s2.getPreview());
-    for (Car_part_priceVO vo : s2.execute()) {
-      System.out.println("> CPP=" + vo);
-    }
-
-  }
+//  private void converterPGArray() {
+//    PlayerVO p = this.playerDAO.select(101);
+//    System.out.println("> Player:" + p.getId() + " cards="
+//        + Stream.of(p.getCards()).map(c -> "" + c).collect(Collectors.joining("|")));
+//    p.setCards(new Integer[] { 7, 8, 8, 9 });
+//    this.playerDAO.update(p);
+//    System.out.println("> Player updated.");
+//  }
+//
+//  private void updateByCriteria() {
+//    System.out.println("> updateByCriteria()");
+//    Car_part_priceTable p = Car_part_priceDAO.newTable("p");
+//
+//    Car_part_priceVO updateValues = new Car_part_priceVO();
+//    updateValues.set_discount(250);
+//    updateValues.setPrice_dollar(150);
+//    UpdateSetCompletePhase u = this.cppDAO.update(updateValues, p, p.part_.eq(1));
+//    System.out.println("Update SQL:" + u.getPreview());
+//    u.execute();
+//
+//    for (Car_part_priceVO vo : this.cppDAO.select(p, p.part_.eq(p.part_)).execute()) {
+//      System.out.println("> CPP=" + vo);
+//    }
+//
+//  }
+//
+//  private void deleteByCriteria() {
+//    System.out.println("> deleteByCriteria()");
+//    Car_part_priceTable p = Car_part_priceDAO.newTable("p");
+//
+//    Car_part_priceVO updateValues = new Car_part_priceVO();
+//    updateValues.set_discount(250);
+//    updateValues.setPrice_dollar(150);
+//    DeleteWherePhase d = this.cppDAO.delete(p, p.part_.eq(2));
+//    System.out.println("Delete SQL:" + d.getPreview());
+//    d.execute();
+//
+//    CriteriaWherePhase<Car_part_priceVO> s2 = this.cppDAO.select(p, p.part_.eq(p.part_));
+//    System.out.println("Select SQL:" + s2.getPreview());
+//    for (Car_part_priceVO vo : s2.execute()) {
+//      System.out.println("> CPP=" + vo);
+//    }
+//
+//  }
 
   private void converter() {
 //    System.out.println("Employee:" + this.employeeDAO.select(123));
