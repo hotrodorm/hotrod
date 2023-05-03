12b6d8c924a39e8d8653e93d4ac39511c3007d92
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2023-05-03 13:56:35 -0400

    Fixing object factory (dealing with interfaces).

--- hotrod-project/hotrod/src/main/java/org/hotrod/runtime/spring/SpringBeanObjectFactory.java
+++ hotrod-project/hotrod/src/main/java/org/hotrod/runtime/spring/SpringBeanObjectFactory.java
@@ -2,8 +2,12 @@ package org.hotrod.runtime.spring;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
+import java.util.SortedSet;
 
+import org.apache.ibatis.reflection.ReflectionException;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
@@ -26,6 +30,14 @@ public class SpringBeanObjectFactory extends DefaultObjectFactory implements App
   public <T> T create(Class<T> type) {
     if (this.isCollection(type)) {
       return super.create(type);
+    } else if (type == List.class || type == Collection.class || type == Iterable.class || type == Map.class
+        || type == SortedSet.class || type == Set.class) {
+      Class<?> clazz = super.resolveInterface(type);
+      try {
+        return (T) clazz.newInstance();
+      } catch (InstantiationException | IllegalAccessException e) {
+        throw new ReflectionException("Error instantiating " + type + " as a " + clazz.getName() + ". Cause: " + e, e);
+      }
     } else {
       T bean = this.applicationContext.getBean(type);
       return bean;
