fb77380178935190d59d6b7e92a106496046dc53
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2023-08-21 10:39:38 -0400

    Configurable bean/pojo instantiation.

--- hotrod-project/hotrod/src/main/java/org/hotrod/runtime/spring/SpringBeanObjectFactory.java
+++ hotrod-project/hotrod/src/main/java/org/hotrod/runtime/spring/SpringBeanObjectFactory.java
@@ -1,74 +1,74 @@
 package org.hotrod.runtime.spring;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.SortedSet;
 
-import org.apache.ibatis.reflection.ReflectionException;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 import org.hotrod.runtime.livesql.Row;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
+@Configuration(proxyBeanMethods = false)
 public class SpringBeanObjectFactory extends DefaultObjectFactory implements ApplicationContextAware {
 
   private static final long serialVersionUID = 1L;
 
   private ApplicationContext applicationContext;
 
+  @Value("${hotrod.vo.instantiation:bean}")
+  private String voInstantiation;
+
+  private enum VOInstantiationType {
+    BEAN, POJO
+  };
+
+  private VOInstantiationType voInstantiationType;
+
   @Override
   public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
     this.applicationContext = applicationContext;
+
+    // parse voInstantiation
+
+    if (this.voInstantiationType == null) {
+      if ("bean".equals(this.voInstantiation)) {
+        this.voInstantiationType = VOInstantiationType.BEAN;
+      } else if ("pojo".equals(this.voInstantiation)) {
+        this.voInstantiationType = VOInstantiationType.POJO;
+      } else {
+        throw new RuntimeException("Invalid value '" + this.voInstantiation
+            + "' in property 'hotrod.vo.instantiation'. Must be either 'bean' (default) or 'pojo'.");
+      }
+      System.out.println("[Object Factory (" + System.identityHashCode(this) + ") - HotRod Instantiation Type: "
+          + this.voInstantiationType + "]");
+    }
+
   }
 
   @Override
   public <T> T create(final Class<T> type) {
-    if (this.isCollection(type)) {
+    if (this.voInstantiationType == VOInstantiationType.BEAN) {
+      // Try to instantiate as a bean
+      if (this.isCollection(type) || type == List.class || type == Collection.class || type == Iterable.class
+          || type == Map.class || type == SortedSet.class || type == Set.class || type == Row.class) {
+        // Cannot be instantiated as a bean. Will be a POJO anyway
+        return super.create(type);
+      } else {
+        // Instantiate a bean
+        return this.applicationContext.getBean(type);
+      }
+    } else {
+      // will always instantiate as a POJO, per configuration setting
       return super.create(type);
-    } else if (type == List.class || type == Collection.class || type == Iterable.class || type == Map.class
-        || type == SortedSet.class || type == Set.class || type == Row.class) {
-      Class<?> clazz = super.resolveInterface(type);
-      return instantiatePOJO(type, clazz);
-    } else {
-      T bean = this.applicationContext.getBean(type);
-      return bean;
     }
-  }
-
-  @SuppressWarnings("unchecked")
-  private <T> T instantiatePOJO(final Class<T> type, final Class<?> clazz) {
-    try {
-      return (T) clazz.getDeclaredConstructor().newInstance();
-    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-        | NoSuchMethodException | SecurityException e) {
-      throw new ReflectionException("Error instantiating " + type + " as a " + clazz.getName() + ". Cause: " + e, e);
-    }
-  }
-
-  @Override
-  public <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
-    if (this.isCollection(type)) {
-      return super.create(type, constructorArgTypes, constructorArgs);
-    } else {
-      return this.applicationContext.getBean(type, constructorArgs);
-    }
-  }
-
-  @Override
-  public void setProperties(Properties properties) {
-    super.setProperties(properties);
-  }
-
-  @Override
-  public <T> boolean isCollection(Class<T> type) {
-    return Collection.class.isAssignableFrom(type);
   }
 
 }
