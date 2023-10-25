2a738bce5669e564a28edfe943403253ebab0e46
valarcon <valarcon@example.com>
2023-10-24 22:53:40 -0400

    docs.

--- hotrod-project/hotrod-livesql/src/main/java/org/hotrod/runtime/livesql/queries/select/ExecutableSelect.java
+++ hotrod-project/hotrod-livesql/src/main/java/org/hotrod/runtime/livesql/queries/select/ExecutableSelect.java
@@ -2,7 +2,7 @@ package org.hotrod.runtime.livesql.queries.select;
 
 /**
  * This class is scheduled for removal in the near future. Use
- * <code>Select</code> instead.
+ * <code>org.hotrod.runtime.livesql.queries.select.Select</code> instead.
  * 
  * @author valarcon
  *
