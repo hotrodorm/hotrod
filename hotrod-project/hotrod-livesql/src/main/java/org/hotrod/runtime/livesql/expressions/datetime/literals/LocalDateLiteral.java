c49359895d8388692864c93a941bb3f3ca37877a
valarcon <valarcon@example.com>
2023-10-28 08:57:55 -0400

    Clean up.

--- hotrod-project/hotrod-livesql/src/main/java/org/hotrod/runtime/livesql/expressions/datetime/literals/LocalDateLiteral.java
+++ hotrod-project/hotrod-livesql/src/main/java/org/hotrod/runtime/livesql/expressions/datetime/literals/LocalDateLiteral.java
@@ -7,14 +7,14 @@ import org.hotrod.runtime.livesql.queries.LiveSQLContext;
 
 public class LocalDateLiteral extends DateTimeLiteral {
 
-  private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+  private static final DateTimeFormatter ISO_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
   // Constructor
 
   public LocalDateLiteral(final LiveSQLContext context, final LocalDate value) {
     super();
-    String iso = value.format(FMT);
-    this.formatted = context.getLiveSQLDialect().getDateTimeLiteralRenderer().renderDate(iso);
+    String isoDate = value.format(ISO_DATE_FORMATTER);
+    this.formatted = context.getLiveSQLDialect().getDateTimeLiteralRenderer().renderDate(isoDate);
   }
 
 }
