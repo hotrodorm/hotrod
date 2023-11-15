732034f5dbd3def4668e4a9c8d9d985df9c6ec0f
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2023-11-15 16:54:12 -0500

    HyperSQL plan implemented. Derby plan removed.

--- hotrod-project/hotrod-torcs/src/main/java/org/hotrod/torcs/plan/ApacheDerbyPlanRetriever.java
+++ hotrod-project/hotrod-torcs/src/main/java/org/hotrod/torcs/plan/ApacheDerbyPlanRetriever.java
@@ -1,11 +1,6 @@
 package org.hotrod.torcs.plan;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import javax.sql.DataSource;
 
 import org.hotrod.torcs.QueryExecution;
 
@@ -13,22 +8,8 @@ public class ApacheDerbyPlanRetriever implements PlanRetriever {
 
   @Override
   public String getEstimatedExecutionPlan(final QueryExecution execution) throws SQLException {
-    DataSource ds = execution.getDataSourceReference().getDataSource();
-    try (Connection conn = ds.getConnection();) {
-      conn.setAutoCommit(false);
-      try (PreparedStatement ps = conn.prepareStatement("explain " + execution.getSQL());
-          ResultSet rs = ps.executeQuery();) {
-        StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        while (rs.next()) {
-          sb.append((first ? "" : "\n") + rs.getString(1));
-          first = false;
-        }
-        return sb.toString();
-      } finally {
-        conn.rollback();
-      }
-    }
+    throw new UnsupportedOperationException(
+        "Apache Derby does not include functionality to provide query execution plans.");
   }
 
 }
--- hotrod-project/hotrod-torcs/src/main/java/org/hotrod/torcs/plan/HyperSQLPlanRetriever.java
+++ hotrod-project/hotrod-torcs/src/main/java/org/hotrod/torcs/plan/HyperSQLPlanRetriever.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.hotrod.torcs.QueryExecution;
-import org.hotrod.torcs.rankings.RankingEntry;
 
 public class HyperSQLPlanRetriever implements PlanRetriever {
 
@@ -17,15 +16,16 @@ public class HyperSQLPlanRetriever implements PlanRetriever {
     DataSource ds = execution.getDataSourceReference().getDataSource();
     try (Connection conn = ds.getConnection();) {
       conn.setAutoCommit(false);
-      try (PreparedStatement ps = conn.prepareStatement("explain " + execution.getSQL());
-          ResultSet rs = ps.executeQuery();) {
-        StringBuilder sb = new StringBuilder();
-        boolean first = true;
-        while (rs.next()) {
-          sb.append((first ? "" : "\n") + rs.getString(1));
-          first = false;
+      try (PreparedStatement ps = conn.prepareStatement("explain plan for\n" + execution.getSQL());) {
+        try (ResultSet rs = ps.executeQuery();) {
+          StringBuilder sb = new StringBuilder();
+          boolean first = true;
+          while (rs.next()) {
+            sb.append((first ? "" : "\n") + rs.getString(1));
+            first = false;
+          }
+          return sb.toString();
         }
-        return sb.toString();
       } finally {
         conn.rollback();
       }
