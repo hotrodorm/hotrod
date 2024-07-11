786f381ad82c11c6f1921e09182922583e5e3e7f
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2024-07-11 12:25:10 -0400

    clean up.

--- hotrod-project/hotrod/src/main/java/org/hotrod/runtime/converter/TypeConverter.java
+++ hotrod-project/hotrod/src/main/java/org/hotrod/runtime/converter/TypeConverter.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
  * 
  *         <p>
  *         Converts back and forth between raw database values and application
- *         values.
+ *         domain values.
  *         </p>
  * 
  *         <p>
@@ -18,36 +18,35 @@ import java.sql.SQLException;
  *         </p>
  * 
  *         <p>
- *         The <b>application</b> value is the value the application uses after
- *         the converter is applied to the raw value.
+ *         The application <b>domain</b> value is the value the application uses
+ *         after the converter is applied to the raw value.
  *         </p>
  *
  *         <p>
  *         For example, a database column DECIMAL(4) can be used to represent a
  *         java.lang.Boolean type (that a database does not support natively)
  *         using the numeric values zero (0) and one (1). This database column
- *         value can be <b>first</b> read through JDBC as a java.lang.Short.
- *         <b>Then</b>, it can be converted into a java.lang.Boolean. In this
- *         case:
+ *         value can be <b>first</b> read through JDBC as a raw java.lang.Short
+ *         value. <b>Then</b>, it can be converted into a java.lang.Boolean. In
+ *         this case:
  *         </p>
  * 
  *         <ul>
  *         <li>DECIMAL(4): the database column type.</li>
- *         <li>java.lang.Short: the type of the intermediate value. This value
- *         is not available to the application and is briefly used by the
+ *         <li>java.lang.Short: the raw type. This value of this type is not
+ *         available to the application and is only briefly used by the
  *         persistence layer while reading/writing from/into the database.</li>
- *         <li>java.lang.Boolean: the type of the application value. This is the
- *         value that the application sees, and is usually stored as a class
- *         property of this type.</li>
+ *         <li>java.lang.Boolean: the domain value. This is the value that the
+ *         application sees.</li>
  *         </ul>
  *
  * @param <R> The raw type read/written to the database.
- * @param <A> The application type.
+ * @param <A> The domain (application) type.
  */
 public interface TypeConverter<R, A> {
 
   A decode(R raw, Connection conn) throws SQLException;
 
-  R encode(A value, Connection conn) throws SQLException;
+  R encode(A domain, Connection conn) throws SQLException;
 
 }
