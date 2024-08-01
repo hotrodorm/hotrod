55a00858cb2430d9f8e722f532fe4c1fe7d0636c
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2024-08-01 11:17:35 -0400

    tests.

--- tests/test-livesql-default-types/src/main/java/app/App.java
+++ tests/test-livesql-default-types/src/main/java/app/App.java
@@ -20,7 +20,6 @@ import org.hotrod.runtime.livesql.queries.ctes.RecursiveCTE;
 import org.hotrod.runtime.livesql.queries.select.CriteriaForUpdatePhase;
 import org.hotrod.runtime.livesql.queries.select.EntitySelect;
 import org.hotrod.runtime.livesql.queries.select.Select;
-import org.hotrod.runtime.livesql.queries.subqueries.Subquery;
 import org.hotrod.runtime.spring.SpringBeanObjectFactory;
 import org.hotrod.torcs.Torcs;
 import org.hotrod.torcs.plan.CouldNotRetrievePlanException;
@@ -183,50 +182,50 @@ public class App {
 
   private void locking() {
 
-//    InvoiceTable i = this.invoiceDAO.newTable();
-//    InvoiceTable j = this.invoiceDAO.newTable();
-////    Subquery x = sql.select(
-////        sql.select(j.id).from(j).where(j.amount.ge(500)).orderBy(j.orderDate.desc()).limit(1));
-//
-//    Select<Row> q = this.sql.select() //
-//        .from(i) //
-//        .where(i.id.in(sql.select(j.id).from(j).where(j.amount.ge(500)).orderBy(j.orderDate.desc()).limit(1))
-//        ) //
-////        .forUpdate().skipLocked() //
-////    .forUpdate().noWait() //
-//        .forUpdate().wait(2) //
-//    ;
-//    System.out.println("q:" + q.getPreview());
-//    List<Row> rows = q.execute();
-////    Row r= q.executeOne();
-////    Cursor<Row> crows = q.executeCursor();
-//    rows.forEach(x -> System.out.println("inv:" + x));
-
     InvoiceTable i = this.invoiceDAO.newTable();
     InvoiceTable j = this.invoiceDAO.newTable();
+//    Subquery x = sql.select(
+//        sql.select(j.id).from(j).where(j.amount.ge(500)).orderBy(j.orderDate.desc()).limit(1));
 
-    // Oracle
-//    EntitySelect<InvoiceVO> q = this.invoiceDAO
-//        .select(i, i.id.in(sql.select(j.id).from(j).where(j.amount.ge(500)).orderBy(j.orderDate.desc()).limit(1))) //
+    Select<Row> q = this.sql.select() //
+        .from(i) //
+        .where(i.id.in(sql.select(j.id).from(j).where(j.amount.ge(500)).orderBy(j.orderDate.desc()).limit(1))) //
+      .forUpdate() //
+//        .forUpdate().skipLocked() //
+//    .forUpdate().noWait() //
+//        .forUpdate().wait(2) //
+    ;
+    System.out.println("q:" + q.getPreview());
+    List<Row> rows = q.execute();
+//    Row r= q.executeOne();
+//    Cursor<Row> crows = q.executeCursor();
+    rows.forEach(x -> System.out.println("inv:" + x));
+
+//    InvoiceTable i = this.invoiceDAO.newTable();
+//    InvoiceTable j = this.invoiceDAO.newTable();
+//
+//    // Oracle
+////    EntitySelect<InvoiceVO> q = this.invoiceDAO
+////        .select(i, i.id.in(sql.select(j.id).from(j).where(j.amount.ge(500)).orderBy(j.orderDate.desc()).limit(1))) //
+////        .forUpdate().skipLocked()
+//////        .forUpdate().noWait()
+//////        .forUpdate().wait(5)
+////    ;
+//    
+//    // PostgreSQL, H2
+//    EntitySelect<InvoiceVO> q = this.invoiceDAO //
+//        .select(i, i.amount.ge(2000)) //
+//        .orderBy(i.orderDate.desc()) //
+//        .limit(1) //
 //        .forUpdate().skipLocked()
 ////        .forUpdate().noWait()
 ////        .forUpdate().wait(5)
 //    ;
-    
-    // PostgreSQL, H2
-    EntitySelect<InvoiceVO> q = this.invoiceDAO //
-        .select(i, i.amount.ge(2000)) //
-        .orderBy(i.orderDate.desc()) //
-        .limit(1) //
-        .forUpdate().skipLocked()
-//        .forUpdate().noWait()
-//        .forUpdate().wait(5)
-    ;
-    
-    System.out.println("q:" + q.getPreview());
-    List<InvoiceVO> cases = q.execute();
-    cases.forEach(x -> System.out.println("inv:" + x));
-
+//    
+//    System.out.println("q:" + q.getPreview());
+//    List<InvoiceVO> cases = q.execute();
+//    cases.forEach(x -> System.out.println("inv:" + x));
+//
   }
 
   private void crudSelect() {
