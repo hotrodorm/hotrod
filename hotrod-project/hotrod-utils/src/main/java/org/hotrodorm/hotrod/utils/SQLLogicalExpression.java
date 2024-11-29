620863667a079a0a65ab5c6ec015a5fb890ed422
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2024-11-29 13:12:00 -0500

    Cleanup.

--- hotrod-project/hotrod-utils/src/main/java/org/hotrodorm/hotrod/utils/SQLLogicalExpression.java
+++
@@ -1,9 +0,0 @@
-package org.hotrodorm.hotrod.utils;
-
-import java.util.Map;
-
-public interface SQLLogicalExpression {
-  public String render();
-
-  public Map<String, Object> getParameters();
-}
