984a92e422f76636aecbaac413bb27637b2dc0f2
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2025-05-18 15:25:52 -0400

    Removing archetype module.

--- hotrod-project/hotrod-archetype-sm-jar-app/src/main/resources/archetype-resources/src/main/database/1.0.0/build.sql
+++
@@ -1,8 +0,0 @@
-create table employee (
-  id number(9) primary key not null, 
-  name varchar2(20)
-);
-
-insert into employee (id, name) values (45, 'Anne');
-insert into employee (id, name) values (123, 'Alice');
-insert into employee (id, name) values (6097, 'Steve');
