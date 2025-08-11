e0bb6e20b7eb90f7c5f2cad2fade5808f07190cb
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2025-08-10 22:26:09 -0400

    Enabling tuples.

--- hotrod-project/hotrod-livesql/src/main/java/org/hotrod/livesql/queries/select/SelectColumnsPhase.java
+++ hotrod-project/hotrod-livesql/src/main/java/org/hotrod/livesql/queries/select/SelectColumnsPhase.java
@@ -30,14 +30,14 @@ public class SelectColumnsPhase<R> extends IndividualSelectPhase<R> {
     s.setResultSetColumns(Arrays.asList(resultSetColumns).stream().collect(Collectors.toList()));
   }
 
-//  // Switching to tuples query
-//
-//  public SelectTuplesColumnsPhase tuples() {
-//    BaseSelectObject<R> select = super.combined.getLastSelect();
-//    List<SQLExpression> cols = select.getSQLExpressions();
-//    SQLExpression[] colsa = cols == null ? null : cols.toArray(new SQLExpression[0]);
-//    return new SelectTuplesColumnsPhase(this.context, select.getCTEs(), select.getDistinct(), colsa);
-//  }
+  // Switching to tuples query
+
+  public SelectTuplesColumnsPhase tuples() {
+    BaseSelectObject<R> select = super.combined.getLastSelect();
+    List<SQLExpression> cols = select.getSQLExpressions();
+    SQLExpression[] colsa = cols == null ? null : cols.toArray(new SQLExpression[0]);
+    return new SelectTuplesColumnsPhase(this.context, select.getCTEs(), select.getDistinct(), colsa);
+  }
 
   // Next phases
 
