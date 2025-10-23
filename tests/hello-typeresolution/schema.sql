7285635a3dbc1f11a71b92c93177394ce0eaf8da
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2025-10-23 10:33:03 -0400

    cleanup.

---
+++ tests/hello-typeresolution/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE invoice (
+  amount DECIMAL(12, 2),
+  status INT,
+  created TIMESTAMP,
+  active CHAR(1) CHECK (active IN ('Y', 'N') ),
+  category DECIMAL(4)
+);
+
+INSERT INTO invoice (amount, status, created, active, category) VALUES
+  (110.05, 3, '2022-09-14 12:30:21', 'Y', 1015),
+  (51.99,  2, '2023-07-22 14:31:35', 'N', 2001),
+  (480.14, 1, '2024-02-03 07:32:23', 'Y', 1018),
+  (224.01, 3, '2025-10-18 20:33:04', 'N', 1023);
