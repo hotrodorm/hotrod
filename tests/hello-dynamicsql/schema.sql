3a63ff48af2e8e0382eb3fe9d28042eab2dd66b0
hotrodorm@protonmail.com <hotrodorm@protonmail.com>
2026-05-19 12:18:04 -0400

    cleanup.

--- tests/hello-dynamicsql/schema.sql
+++ tests/hello-dynamicsql/schema.sql
@@ -8,15 +8,6 @@ create table employee (
   title varchar(30)  
 );
 
--- 1. Two Smith last name
--- 2. Ordering by last name
--- 3. Selecting Ids 105, 1077, and 518
--- 4. city 14, title "Medic"; city 27, title "Virtual Assistant"
--- 5. title "Medic", one other vip
--- 6. --
--- 7. 2x First name "Peter" hired after 2025-06-12
--- 8. 2x city = 27, status "P" 
-
 insert into employee (id, first_name, last_name, hired_on, active, city_id, title) values
   (101, 'Peter', 'Smith', '2026-01-12', 'A', 14, 'Medic'),
   (105, 'Marie', 'Jonas', '2021-02-15', 'P', 14, 'Virtual Assistant'),
@@ -25,5 +16,3 @@ insert into employee (id, first_name, last_name, hired_on, active, city_id, titl
   (640, 'Lucy', 'Barr', '2021-02-15', 'P', 27, 'Clerk'),
   (1077, 'Lucas', 'Santon', '2021-02-15', 'P', 27, 'Virtual Assistant'),
   (1241, 'Peter', 'Arunsen', '2025-09-17', 'A', 30, 'Office Manager');
-
-  
